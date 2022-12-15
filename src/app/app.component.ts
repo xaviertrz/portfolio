7b1113b9eb7af4e3cffda7c159f97cd83c81c62e
xaviertar <xavtarazona@gmail.com>
2022-12-15 00:05:08 -0500

    Implement SVG icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-// @ts-ignore
-import Typewriter from 't-writer.js';
+
+import { writer } from './tw';
+
 
 @Component({
   selector: 'app-root',
@@ -11,20 +12,6 @@ export class AppComponent {
   constructor() {}
 
   ngOnInit(): void {
-    const target = document.querySelector('.tw');
-    const writer = new Typewriter(target, {
-      loop: true,
-      typeSpeed: 80,
-      deleteSpeed: 80,
-      typeColor: '#888888',
-      cursorColor: '#A9A9A9',
-    });
-
-    const greetings = ['console.log("Hello, world!");', 'print("Hello, world!")', 'echo "Hello, world!";', 'puts "Hello, world!";', 'System.out.println("Hello, world!");', 'Console.WriteLine("Hello, world!");'];
-    const time = 2000;
-
-    greetings.forEach((greeting) => {
-      writer.type(greeting).rest(time).remove(greeting.length).start();
-    });
+    writer();
   }
 }
